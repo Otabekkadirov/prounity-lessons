d868054e7fb694ce95ef05054e121b50c165f89f
Otabek <ganijanovich94@gmail.com>
2022-11-21 17:40:28 +0500

    1-guruh darslari: JavaScript kirish

---
+++ Frontend 1-guruh/JavaScript/main.js
@@ -0,0 +1,56 @@
+// alert("Hello world");
+// console.log("Hello world");
+// console.error("AAAAAA!!!!");
+// console.warn("Ehtiyot bo'l");
+
+/* Data types:
+string, number, boolean, object, undefined, null and symbol
+*/
+
+// console.log(String); ***
+// console.log(Number); ***
+// console.log(Boolean); ***
+// console.log(Object); **
+// console.log(undefined); **
+// console.log(null); **
+// console.log(Symbol); *
+
+// console.log("Otabek");
+// console.log("Otabek");
+
+// console.log("O'ktam");
+// console.log('Bobom dedi: "Kel, ertak aytib beray."');
+// console.log('Bobom dedi: "Kel O\'ktam, ertak aytib beray."');
+
+// console.log("O'zbekiston");
+// console.log('"O\'zbekiston"');
+
+// console.log(`"O'zbekiston"`); // backtick
+
+// console.log("Otabek Kadirov");
+// console.log("Yoshim 29 da");
+// console.log("29");
+// console.log("console.log()");
+
+// console.log(29);
+// console.log("29");
+
+// console.log(29 + 1);
+// console.log(29 - 5);
+// console.log(20 * 2);
+// console.log(20 / 2);
+// console.log(34 + (45 / 2) * 5 + 62);
+
+// console.log(100 / 3);
+// console.log(0.1 + 0.2);
+
+// console.log("Otabek", 29);
+
+// console.log("Mening ismim Otabek. Yoshim", 29, "da.");
+
+// var yosh = 30;
+
+// console.log(`Mening ismim Otabek. Yoshim ${yosh} da.`);
+
+// console.log(true);
+// console.log(false);
