e1da9b0b61876b82dea792ebe094236015f159f8
Otabek <ganijanovich94@gmail.com>
2022-11-26 13:12:52 +0500

    1-guruh darslari: JS var, let, const

---
+++ Frontend 1-guruh/JavaScript/string.js
@@ -0,0 +1,34 @@
+// let x = "London";
+// x = "Manchester";
+
+// let z = 52;
+// z = 10;
+
+// let y = true;
+
+// let xabar = `Britaniyaning poytaxti ${x}, aholi soni ${z} million.`;
+
+// console.log(xabar);
+
+// let salary = 2000;
+// salary = 4000;
+
+// let expense = (salary * 75) / 100;
+
+// console.log(expense);
+
+// var, let, const
+
+// let myName;
+// let myAge;
+// const myHometown = "Buxoro";
+// var mySalary = 500;
+// myAge = 29;
+// myName = "Otabek";
+// myAge += 1;
+
+// let myMessage = `Ismim ${myName}, yoshim ${myAge} da. Ona shahrim ${myHometown}. Keyingi yil oyligim ${
+//     mySalary * 0.5 + "0"
+// }$ bo'ladi`;
+
+// console.log(myMessage.length);
