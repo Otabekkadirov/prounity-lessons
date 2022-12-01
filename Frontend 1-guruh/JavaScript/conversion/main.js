67173d8bebb3b9ef1e4d9fec79da26b1ef43605e
Otabek <ganijanovich94@gmail.com>
2022-12-01 13:59:35 +0500

    1-guruh darslari: Type conversion in JS

---
+++ Frontend 1-guruh/JavaScript/conversion/main.js
@@ -0,0 +1,87 @@
+// console.log(`Hello ning turi `, typeof "Hello"); // string
+// console.log(`123 ning turi `, typeof 123); // number
+// console.log(`true ning `, typeof true); // boolean
+
+// let x = null;
+
+// console.log(`x ning turi `, typeof x); // null
+
+// let i = undefined;
+
+// console.log(`i ning turi `, typeof i); // undefined
+
+// let person = { name: "Javohir", age: 15 };
+
+// console.log(`person ning turi `, typeof person); // object
+
+// let bar = true;
+// console.log(`true   ning turi:`, typeof bar);
+
+// let myAge = -29;
+
+// myAge = +myAge;
+
+// console.log(myAge);
+
+// console.log(2 ** 2);
+// console.log(5 ** 4);
+
+// console.log(2 ** 9);
+// console.log(6 ** 6);
+// console.log(6 ** 6);
+
+// console.log(100 ** 11);
+
+// console.log("hello" + " world");
+
+// console.log(+1);
+// console.log(+-1);
+// console.log(+"125");
+// console.log(+"0");
+// console.log(+"Abbos");
+// console.log(+" ");
+
+// let myName = true;
+
+// console.log(+myName);
+
+// let apples = "2";
+// let oranges = "3";
+
+// console.log((+apples) ** +oranges);
+
+// let x = 2 - 2 * 1;
+
+// console.log(x);
+
+// let a, b, c;
+
+// a = b = c = 2 + 2;
+
+// console.log(a * b ** c);
+
+// let n = 2;
+// n += 5;
+// n *= 2;
+
+// console.log(n);
+
+// let n = 100;
+// n ++; // 101
+
+// let myAge = 29;
+
+// myAge++; // myAge += 1, myAge = myAge + 1
+
+// myAge = myAge - 1
+// myAge -= 1;
+// myAge--
+
+// let myName = (1, 2, 3, 4, 5, 23);
+// console.log(myName);
+
+let b = 7;
+
+console.log(++b);
+console.log(b++);
+console.log(b);
