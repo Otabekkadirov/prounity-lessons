113bf07f0135b4bc93f34abd1a03208a838a28b0
Otabek <ganijanovich94@gmail.com>
2022-12-03 13:51:12 +0500

    1-guruh darslari: Comparison operators in JS

---
+++ Frontend 1-guruh/JavaScript/comparison/main.js
@@ -0,0 +1,40 @@
+// Comparison
+
+// console.log(2 > 1);
+
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 == 2);
+// console.log(2 === 2);
+
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 >= 2);
+// console.log(2 > 2);
+
+// console.log(0 == false);
+
+// let a = "0";
+// a = Number(a);
+
+// let b = false;
+// b = String(b);
+
+// console.log(a === b);
+
+// console.log(true == "true");
+
+// let a = true;
+// a = String(a); // => "true"
+// a = Number(a); // => NaN
+
+// let b = "true";
+// b = Number(b); // => NaN
+// console.log(a == b);
+
+// console.log(Infinity == Infinity);
+
+// console.log(Number(Infinity));
+// console.log(undefined === null);
+// console.log(undefined === undefined);
+// console.log(null == null);
