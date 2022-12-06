d91a3911592c8e0a5b50ece57ab10d765bfe856d
Otabek <ganijanovich94@gmail.com>
2022-12-06 11:22:01 +0500

    1-guruh darslari: conditionals in JS

---
+++ Frontend 1-guruh/JavaScript/conditional/main.js
@@ -0,0 +1,34 @@
+// Conditionals
+
+// let userName = prompt("Ismingiz nima?");
+// let isBobur = userName == "Bobur";
+
+// if (isBobur) {
+//     console.log(`Salom ${userName} ustoz!`);
+// } else {
+//     console.log(`Salom ${userName}!`);
+// }
+
+// let year = prompt("O'zbekiston qachon mustaqillikka erishgan?");
+
+// if (year < 1991) {
+//     alert("Juda vaqtli");
+// } else if (year > 1991) {
+//     alert("Juda kech");
+// } else if (year == 1991) {
+//     alert("To'g'ri");
+// } else {
+//     alert("Iltimos son kiritng!");
+// }
+
+// let userName = prompt("Ismingizni kiriting!");
+
+// if (userName == "") {
+//     console.log(`Salom foydalanuvchi`);
+// } else {
+//     console.log(`Salom ${userName}`);
+// }
+
+// userName == ""
+//     ? console.log(`Salom foydalanuvchi`)
+//     : console.log(`Salom ${userName}`);
