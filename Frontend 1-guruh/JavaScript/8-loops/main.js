12cdd2ebe16c86591678fb00c56317d4ad7bb121
Otabek <ganijanovich94@gmail.com>
2022-12-12 18:03:10 +0500

    1-guruh darslari: js loops

---
+++ Frontend 1-guruh/JavaScript/8-loops/main.js
@@ -0,0 +1,25 @@
+// Loops
+console.log("Hello loops\n");
+
+// let son = 100;
+
+// while (son) {
+//     console.log(`sonimiz ${son} ga teng`);
+//     son = son - 10;
+// }
+
+// do {
+//     console.log(`sonimiz ${son} ga teng`);
+//     son = son - 10;
+// } while (son);
+
+// let salary = 0;
+
+// do {
+//     console.log("Men ishlayapman.");
+//     salary = salary + 500;
+// } while (salary <= 10000);
+
+// for (let son = 1; son <= 10; son = son + 1) {
+//     console.log(son);
+// }
