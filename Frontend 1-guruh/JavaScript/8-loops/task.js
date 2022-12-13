eaf854314c3fd578e79a84fd93c9e25c6bb1d878
Otabek <ganijanovich94@gmail.com>
2022-12-13 13:50:56 +0500

    1-guruh darslari: js loops task

---
+++ Frontend 1-guruh/JavaScript/8-loops/task.js
@@ -0,0 +1,17 @@
+// let result = "";
+// for (let i = 1; i <= 10; i++) {
+//     result += i;
+//     console.log(result);
+// }
+
+// for (let i = 1; i <= 30; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log(`${i} - 3 ga va 5 ga bo'linadi`);
+//     } else if (i % 5 == 0) {
+//         console.log(`${i} - 5 ga bo'linadi.`);
+//     } else if (i % 3 == 0) {
+//         console.log(`${i} - 3 ga bo'linadi.`);
+//     } else {
+//         console.log(`${i}`);
+//     }
+// }
