4a66ff3c0fa1e4e0d7967c0cf4355f976d5d14d4
Otabek <ganijanovich94@gmail.com>
2023-01-06 12:27:51 +0500

    1-guruh darslari: iterators

---
+++ Frontend 1-guruh/JavaScript/13-iterators/main.js
@@ -0,0 +1,110 @@
+const artists = ["Picasso", "Kahlo", "Van Ghog", "Utamaro"];
+
+// for (let i = 0; i < artists.length; i++) {
+//     console.log(artists[i]);
+// }
+
+// forEach()
+
+// artists.forEach(log);
+
+// function log(artist) {
+//     console.log(artist);
+// }
+
+const numbers = [1, 2, 3, 4, 5];
+
+// const doubleNumbers = numbers.map(double);
+
+// function double(n) {
+//     return n * 2;
+// }
+
+// const doubleNumbers = numbers.map((number) => number * 2);
+
+// console.log(doubleNumbers);
+
+const students = [
+    "Po'lat",
+    "Rustam",
+    "Oybek",
+    "Umar",
+    "Nodir",
+    "Ilhom",
+    "Temur",
+    "Yigitali",
+];
+
+// let result = "";
+// for (let i = 0; i < students.length; i++) {
+//     result += students[i][0];
+// }
+
+// const result = students.map((student) => {
+//     return student[0];
+// });
+// console.log(result);
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+// const smallNumbers = randomNumbers.filter(number => {
+//   return number < 250;
+// })
+
+const favoriteWords = [
+    "nostalgia",
+    "hyperbole",
+    "fervent",
+    "esoteric",
+    "serene",
+];
+
+// Call .filter() on favoriteWords below
+// const longFavoriteWords = favoriteWords.filter(word => {
+//   return word.length > 7;
+// })
+
+const animals = [
+    "hippo",
+    "tiger",
+    "lion",
+    "seal",
+    "cheetah",
+    "monkey",
+    "salamander",
+    "elephant",
+];
+
+// const foundAnimal = animals.findIndex(animal => {
+//   return animal === 'elephant';
+// })
+
+// const startsWithS = animals.findIndex(animal => {
+//   return animal[0] === 's';
+// })
+
+// console.log(foundAnimal, startsWithS)
+
+const words = ["unique", "uncanny", "pique", "oxymoron", "guise"];
+
+// Something is missing in the method call below
+
+// console.log(words.some((word) => {
+//   return word.length < 6;
+// }));
+
+// Use filter to create a new array
+// const interestingWords = words.filter(word => {
+//   return word.length > 5;
+// })
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+// console.log(interestingWords.every((word) => {
+//   return word.length > 5;
+// } ));
+
+// const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+// const nums = [1, 50, 75, 200, 350, 525, 1000];
